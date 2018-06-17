350ced4b12f1f432f42c061f01c8fd48afa62c6b
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2018-06-17 18:17:34 +0200

    formatting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,25 +3,25 @@ import rocks.xmpp.core.session.XmppClient
 import rocks.xmpp.core.XmppException
 
 object Main extends App {
-    println("Connecting...")
-    val password = ""
-    val client = XmppClient.create("wiuwiu.de")
-    try {
-        client.connect()
-    } catch {
-        case _: XmppException => {
-            println("Connection failed")
-            System.exit(1)
-        }
+  println("Connecting...")
+  val password = ""
+  val client = XmppClient.create("wiuwiu.de")
+  try {
+    client.connect()
+  } catch {
+    case _: XmppException => {
+      println("Connection failed")
+      System.exit(1)
     }
-    try {
-        client.login("marmistrz", password, "rocks")
-    } catch {
-        case _: XmppException => {
-            println("login failed")
-            System.exit(1)
-        }
+  }
+  try {
+    client.login("marmistrz", password, "rocks")
+  } catch {
+    case _: XmppException => {
+      println("login failed")
+      System.exit(1)
     }
+  }
 
-    println("Done")
+  println("Done")
 }
